3f44198929fcbbc96f813ef9687a8a11775b6da1
micheal oshogwe <kenneth.oshogwe@gmail.com>
2023-08-01 08:01:37 -0700

    task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,7 +12,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *newnode;
 
 	newnode = malloc(sizeof(listint_t));
-	if (!newnode)
+	if (newnode == NULL)
 		return (NULL);
 
 	newnode->n = n;
