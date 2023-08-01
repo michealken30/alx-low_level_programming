2d0f6cd864d55e09bedb7408154cf8a15c46785e
micheal oshogwe <kenneth.oshogwe@gmail.com>
2023-08-01 07:59:02 -0700

    task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -16,7 +16,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		return (NULL);
 
 	newnode->n = n;
-	newnode->next = NULL;
+	newnode->next = *head;
 	*head = newnode;
 
 	return (newnode);
